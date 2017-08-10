0024710d991cb8064b16f4329ab6e787617ae04b
yolanda1181119 <yolanda1181119@gmail.com>
2017-08-10 03:40:03 -0400

    first commit

---
+++ static/maze/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1502339161,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg.png",
+		"images/player-sheet0.png",
+		"images/playerskin-sheet0.png",
+		"images/door-sheet0.png",
+		"images/door-sheet1.png",
+		"images/door-sheet2.png",
+		"images/tool-sheet0.png",
+		"images/tool-sheet1.png",
+		"images/unfloor-sheet0.png",
+		"images/undoor-sheet0.png",
+		"images/spritefont.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
